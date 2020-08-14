a9b5c882010f16b292fd0ffbcca0929bf1f3906e
sabbir-320 <sabbir195661@gmail.com>
2020-08-14 12:09:48 +0600

    default

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+const firstName = "Sabbir";
+const lastName = "Ahmed";
+const fullName = `${firstName} ${lastName}`;
+const multiLine =`
+                    Name: Sabbir Ahmed
+                    Father's name: Habibul Haque
+                    Phone number: 01710195661`
+console.log(multiLine);
+class Persons{
+    constructor(name, id, age){
+        this.Name = name;
+        this.Id = id;
+        this.Age = age;
+        this.schoolName = "Baitus Salam Madrasa"
+    }
+}
+console.log(new Persons("sabbir", 2, 3))
+console.log(new Persons("Mukit", 4, 5))
